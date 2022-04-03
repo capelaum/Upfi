da0a0f9ffccfb9d7a623512e3e7da83e347493ec
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-04-03 16:42:17 -0300

    Add Image Form + Infinite Query

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -4,9 +4,16 @@ import { TextInput } from 'components/Input/TextInput';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useMutation, useQueryClient } from 'react-query';
+import { api } from 'services/api';
 
 interface FormAddImageProps {
   closeModal: () => void;
+}
+
+interface ImageFormData {
+  image: unknown;
+  description: string;
+  title: string;
 }
 
 export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
@@ -14,52 +21,94 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   const [localImageUrl, setLocalImageUrl] = useState('');
   const toast = useToast();
 
+  const regexInputImageAcceptedFormats = new RegExp('^image\\/(gif|jpeg|png)$');
+
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: (fileList: FileList) =>
+          fileList[0].size < 10 * 1024 * 1024 ||
+          'O arquivo deve ser menor que 10MB',
+
+        acceptedFormats: (fileList: FileList) =>
+          regexInputImageAcceptedFormats.test(fileList[0].type) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF'
+      }
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: 'Título obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres'
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres'
+      }
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: 'Descrição obrigatória',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres'
+      }
     }
   };
 
   const queryClient = useQueryClient();
   const mutation = useMutation(
-    // TODO MUTATION API POST REQUEST,
-
+    async (data: Record<string, unknown>) => {
+      const response = await api.post('/images', {
+        url: imageUrl,
+        title: data.title,
+        description: data.description
+      });
+      return response.data;
+    },
     {
-      // TODO ONSUCCESS MUTATION
+      onSuccess: () => queryClient.invalidateQueries('images')
     }
   );
 
   const { register, handleSubmit, reset, formState, setError, trigger } =
     useForm();
+
   const { errors, isSubmitting } = formState;
 
   const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
-      if (!imageUrl) {
-        throw new Error('Image URL is required');
+      if (!data.image) {
+        toast({
+          title: 'Imagem não adicionada',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.',
+          status: 'error'
+        });
+
+        return;
       }
 
-      // TODO EXECUTE ASYNC MUTATION
+      await mutation.mutateAsync(data);
 
-      // TODO SHOW SUCCESS TOAST
       toast({
-        title: 'Success',
-        description: 'Image added successfully',
-        status: 'success',
-        duration: 5000,
-        isClosable: true
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+        status: 'success'
       });
+
+      return;
     } catch {
-      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.',
+        status: 'warning'
+      });
     } finally {
-      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      reset(data);
+      setLocalImageUrl('');
+      setImageUrl('');
+      closeModal();
     }
   };
 
@@ -72,20 +121,20 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          {...register('image', formValidations.image)}
+          error={errors.image}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          error={errors.title}
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          error={errors.description}
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,23 @@ import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 import { api } from 'services/api';
 
+interface Card {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+type GetImagesResponse = {
+  data: Card[];
+  after: string | null;
+};
+
 export default function Home(): JSX.Element {
-  const getImages = async ({ pageParam = null }) => {
+  const getImages = async ({
+    pageParam = null
+  }): Promise<GetImagesResponse> => {
     const data = await api
       .get('/images', {
         params: {
@@ -16,8 +31,6 @@ export default function Home(): JSX.Element {
         }
       })
       .then((response) => response.data);
-
-    console.log(data);
 
     return data;
   };
@@ -29,28 +42,17 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage
-  } = useInfiniteQuery(
-    'images',
+  } = useInfiniteQuery('images', getImages, {
+    getNextPageParam: (lastPage) => lastPage.after ?? null
+  });
 
-    // TODO AXIOS REQUEST WITH PARAM
-    getImages,
-
-    // TODO GET AND RETURN NEXT PAGE PARAM
-    {
-      getNextPageParam: (lastPage) => lastPage.after ?? null
-    }
-  );
-
+  // TODO FORMAT AND FLAT DATA ARRAY
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
     return data?.pages.map((page) => page.data).flat();
   }, [data]);
-  console.log('🚀 ~ formattedData', formattedData);
 
-  // TODO RENDER LOADING SCREEN
   if (isLoading) return <Loading />;
 
-  // TODO RENDER ERROR SCREEN
   if (isError) return <Error />;
 
   return (
@@ -58,7 +60,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        <CardList cards={formattedData ?? ([] as Card[])} />
         {hasNextPage}
         {hasNextPage && (
           <Button mt="5" onClick={() => fetchNextPage()}>
